5d77fdff297e880326569903dc3402b62dbef199
G01\abes <abes@fujitsu.com>
2024-06-06 11:20:21 +0900

    フルスクリーン実装試行

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../styles/globals.css";
 import "@fontsource/poppins";
 import { ThemeProvider } from "next-themes";
@@ -8,12 +8,35 @@ import RootLayout from "@/pages/components/layout";
 import type { ServerRouter } from "@/server/router";
 // import HeaderA from "@/pages/components/HeaderA";
 import { EtProvider } from "../providers/EtProvider";
-
+import { useEffect } from 'react';
 
 // eslint-disable-next-line react/prop-types
 const App: AppType = ({ Component, pageProps }) => {
+
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+  useEffect(() => {
+    if (!isFullScreen) {
+      const enterFullScreen = async () => {
+        await document.documentElement.requestFullscreen();
+        setIsFullScreen(true);
+        document.removeEventListener('click', enterFullScreen);
+      };
+      document.addEventListener('click', enterFullScreen);
+      return () => {
+        document.removeEventListener('click', enterFullScreen);
+      };
+    }
+  }, [isFullScreen]);
+
+
   return (
-    <div>
+    <div className="fixed inset-0 ">
+      {!isFullScreen && (
+        <div className="absolute inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
+          <p className="text-white text-2xl"></p>
+        </div>
+      )}
       <ThemeProvider attribute="class" defaultTheme="dark">
         <RootLayout>
           <EtProvider>
